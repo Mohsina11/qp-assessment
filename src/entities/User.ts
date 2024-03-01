9ca149bf1115d14dae34f1844171f3288aa773b7
Mohsina Shaikh <mohaseena.r@exotel.in>
2024-03-02 00:08:40 +0530

    Initial Commit

---
+++ src/entities/User.ts
@@ -0,0 +1,27 @@
+// src/entities/User.ts
+
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity({ name: 'user' })
+export class User {
+  @PrimaryGeneratedColumn()
+  userId: number=0;
+
+  @Column()
+  username: string='';
+
+  @Column({ unique: true })
+  email: string='';
+
+  @Column({ unique: true })
+  phoneNumber: string='';
+
+  @Column()
+  password: string='';
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date=new Date();
+  
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updated_at: Date=new Date();
+}
---
+++ src/routes/user.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import UserController from '../controllers/user';
+
+const router = express.Router();
+const userController = new UserController();
+
+router.get('/viewitems', userController.viewItems);
+router.post('/:userId/bookitems', userController.bookItems);
+
+export default router;
